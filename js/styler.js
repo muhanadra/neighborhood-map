c777968ea518613ec119d0a379c74328193f33c6
Muhanad <neo0rabie@gmail.com>
2015-05-29 21:53:40 -0700

    A simpler responsive layout based of feedreader project

--- js/styler.js
+++ js/styler.js
@@ -1,4 +1,4 @@
-//This file is for our jquery DOM manipulation to make sure the website is responsive
+// //This file is for our jquery DOM manipulation to make sure the website is responsive
 $(document).ready(function() {
     // Our function is going to be loaded once the page loads and whenever the windows size change
     checkSize();
@@ -10,23 +10,18 @@ $(document).ready(function() {
 var mapH; // the height of the map
 //.flex-direction changes using media queries. So we are using this css property to detect media queries.
 function checkSize(){
-    if ($(".scenes-container").css("flex-direction") == "row" ) {
+    mapH = window.innerHeight - ($('.header').height());
+    $('.map').height(mapH);
+    var headerH = $('.header').height();
+    $('.map').css('margin-top', headerH);
+    $('.menu').css('top', headerH);
 
-		mapH = window.innerHeight - ($('.header').height() + 185);
-		$('.map').css("height", mapH + "px");
-		$('.scenes-container').height(175);
-    }
-    if ($(".scenes-container").css("flex-direction") == "column" ) {
-		mapH = window.innerHeight - $('.header').height();
-		$('.map').height(mapH);
-        $('.scenes-container').height(mapH);
-        // Checking for Landscape view
-        if ($(".sidebar").css("position") == "absolute" ) {
-            console.log("landscape");
-
-            $('.map').width(window.innerWidth-$('.scenes-container').width);
-            $('.scenes-container').height(window.innerHeight);
-        }
-		console.log("Map is set to fit column scenes");
-    }
+    var searchH = $('.scenes-container').height();
+	$('.scenes-container').height(mapH - searchH - 5);
 }
+
+// Show our menu and hide it.
+var menuIcon = $('.menu-icon-link');
+menuIcon.on('click', function() {
+        $('body').toggleClass('menu-hidden');
+    });
